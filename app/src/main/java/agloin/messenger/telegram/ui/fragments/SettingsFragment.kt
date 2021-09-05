c7a54244153e0efdf6f7327ddb06a7312c21a94f
agloin <darabeev@gmail.com>
2021-09-05 22:01:25 +0300

    Action bat with action menu is done. I make action menu with icons and names

--- app/src/main/java/agloin/messenger/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/agloin/messenger/telegram/ui/fragments/SettingsFragment.kt
@@ -1,9 +1,16 @@
 package agloin.messenger.telegram.ui.fragments
 
 import agloin.messenger.telegram.R
+import android.view.Menu
+import android.view.MenuInflater
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
 }
